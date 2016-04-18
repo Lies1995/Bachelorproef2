e4e128be09aa676a947f467ee6b9b20d9c5e73d8
Lies Deceuninck <lies.deceuninck@gmail.com>
2016-04-18 18:59:48 +0200

    verslag en afbeeldingen DLS

--- Measurement_Results/DLS_read_2.m
+++ Measurement_Results/DLS_read_2.m
@@ -10,9 +10,12 @@ R = [hex2rgb('#72003E'); hex2rgb('#FE008B'); hex2rgb('#B20061'); hex2rgb('#BF006
 B = [hex2rgb('#063A3F'); hex2rgb('#15BACB'); hex2rgb('#0D747F'); hex2rgb('#0E808C'); hex2rgb('#0A5D65');...
     hex2rgb('#031A1C');hex2rgb('#119AA8');hex2rgb('#09545C');hex2rgb('#0B5F69');hex2rgb('#073D42')];
 
+%----- PLOT data for the different particles-----
 ml=[0.8, 1.24, 2.5];
 for i=1: 3
     A=xlsread(fullfile('DLS','Data_verslag',[num2str(ml(i)) '.xlsx']))
+    C=xlsread(fullfile('DLS','Data_verslag',[num2str(ml(i)) '_20k.xlsx']))
+
     fig=figure(i)
     set(fig, 'Position', [262 191 900 440])
     if i==1
@@ -50,4 +53,27 @@ for i=1: 3
     xlabel('Diameter ($nm$)')
     ylabel('Intensity (a.u.)') %normalized correlation curve
     hold off
+    fig=figure(4)
+    set(fig, 'Position', [262 191 900 440])
+    col=[hex2rgb('#0D747F'); hex2rgb('#B20061'); hex2rgb('#BFAA13') ];
+    hold on
+    if i==2
+    stem(A(1:250,19),A(1:250,20),'.','Color', col(i,:),'Markeredgecolor','white','Markersize',1,'LineWidth',10.0);
+    else
+    stem(A(1:250,21),A(1:250,22),'.', 'Color',col(i,:),'Markeredgecolor','white','Markersize',1,'LineWidth',10.0);
+    end
+    set(gca,'XScale','log');
+    xlabel('Diameter ($nm$)')
+    ylabel('Intensity (a.u.)') %normalized correlation curve
+    hold off
+    fig=figure(5)
+    hold on
+    set(fig, 'Position', [262 191 900 440])
+    stem(C(1:250,21),C(1:250,22),'.','Color',col(i,:),'Markeredgecolor','white','Markersize',1,'LineWidth',10.0);
+    set(gca,'XScale','log');
+    xlabel('Diameter ($nm$)')
+    ylabel('Intensity (a.u.)') %normalized correlation curve
+    hold off 
 end
+
+%---- plot before and after PEG averages particles----
