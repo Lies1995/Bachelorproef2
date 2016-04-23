6162c1966b2c211cdb0fbb58f7bf46833501457b
Lies Deceuninck <lies.deceuninck@gmail.com>
2016-04-23 17:14:35 +0200

    juiste TEM afbeeldingen

--- Measurement_Results/TEM.m
+++ Measurement_Results/TEM.m
@@ -6,15 +6,15 @@ figure(1)
 A=xlsread(fullfile('TEM','0.8.xlsx'))
 h = histogram(A(:,1),50,'FaceColor',hex2rgb('#0D747F'),'EdgeColor',hex2rgb('#063A3F'))
 xlabel('Size (nm)')
-ylabel('Counts');
+ylabel('Number of particles');
 figure(2)
 A=xlsread(fullfile('TEM','1.24.xlsx'))
 h = histogram(A(:,1),50,'FaceColor',hex2rgb('#B20061'),'EdgeColor',hex2rgb('#72003E'))
 xlabel('Size (nm)')
-ylabel('Counts');
+ylabel('Number of particles');
 figure(3)
 A=xlsread(fullfile('TEM','2.5.xlsx'))
 h = histogram(A(:,1),50,'FaceColor',hex2rgb('#BFAA13'),'EdgeColor',hex2rgb('#7F710D'))
 xlabel('Size (nm)')
-ylabel('Counts');
+ylabel('Number of particles');
  
