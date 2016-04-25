3d1705225d616ca1875029e40da6abea286a55e8
Lies Deceuninck <lies.deceuninck@gmail.com>
2016-04-25 15:50:02 +0200

    no message

--- Measurement_Results/gel/GNP.m
+++ Measurement_Results/gel/GNP.m
@@ -10,9 +10,12 @@ p_Au=19.32;%g/cm^3
 
 M_HAuCl4=M_H+M_Au+4*M_Cl;
 m=[ r_15 r_30 r_45];
+c_250=0;
+c_05=0;
 for i=1:length(m)
     r=m(i)*10^(-7);
     V_Au=(4/3)* pi *r^3;
-    N=*( m_HAuCl4 * M_Au ) / ( M_HAuCl4 * p_Au * V_Au);
-    
+    N=( m_HAuCl4 * M_Au ) / ( M_HAuCl4 * p_Au * V_Au);
+    c_250=(N*10^(-3))/250
+    c_05=N*2*10^(-3)
 end
