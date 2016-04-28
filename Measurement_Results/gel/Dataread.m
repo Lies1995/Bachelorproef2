23c7d6ca3a627ccf25d4399ccc86ff2616947ef0
Lies Deceuninck <lies.deceuninck@gmail.com>
2016-04-28 18:24:48 +0200

    gel results

--- Measurement_Results/gel/Dataread.m
+++ Measurement_Results/gel/Dataread.m
@@ -9,50 +9,53 @@ hold on
 plot(x,A(1:4,2),'.','Color','k','Markersize',15) %ref
 plot(x,A(1:4,3),'.','Color',hex2rgb('#FFE319'),'Markersize',15) %1k
 plot(x,A(1:4,4),'.','Color',hex2rgb('#BFAA13'),'Markersize',15) %5k
-plot(x,A(1:4,5),'.','Color',hex2rgb('#A99611'),'Markersize',15) %10k
-plot(x,A(1:4,6),'.','Color',hex2rgb('#87780D'),'Markersize',15) %20k
+plot(x,A(1:4,5),'.','Color',hex2rgb('#87780D'),'Markersize',15) %10k
+plot(x,A(1:4,6),'.','Color',hex2rgb('#423B07'),'Markersize',15) %20k
 plot(x,A(1:4,2),'Color','k','Markersize',15) %ref
 plot(x,A(1:4,3),'Color',hex2rgb('#FFE319'),'Markersize',15) %1k
 plot(x,A(1:4,4),'Color',hex2rgb('#BFAA13'),'Markersize',15) %5k
-plot(x,A(1:4,5),'Color',hex2rgb('#A99611'),'Markersize',15) %10k
-plot(x,A(1:4,6),'Color',hex2rgb('#87780D'),'Markersize',15) %20k
+plot(x,A(1:4,5),'Color',hex2rgb('#87780D'),'Markersize',15) %10k
+plot(x,A(1:4,6),'Color',hex2rgb('#423B07'),'Markersize',15) %20k
 
 hold off
 set(gca, 'XTick',1:5, 'XTickLabel',{'$0$Gy' '$5$Gy' '$10$Gy' '$15$Gy'})
 axis([0 6 10 80])
 ylabel('Damage ($\%$)')
+legend('Ref','1k','5k','10k','20k');
 
 figure(2) %30nm
 hold on
 plot(x,A(7:10,2),'.','Color','k','Markersize',15) %ref
-plot(x,A(7:10,3),'.','Color',hex2rgb('#FF008B'),'Markersize',15) %1k
-plot(x,A(7:10,4),'.','Color',hex2rgb('#D10072'),'Markersize',15) %5k
+plot(x,A(7:10,3),'.','Color',hex2rgb('#F7AAE4'),'Markersize',15) %1k
+plot(x,A(7:10,4),'.','Color',hex2rgb('#FF0197'),'Markersize',15) %5k
 plot(x,A(7:10,5),'.','Color',hex2rgb('#B20061'),'Markersize',15) %10k
-plot(x,A(7:10,6),'.','Color',hex2rgb('#9C0055'),'Markersize',15) %20k
+plot(x,A(7:10,6),'.','Color',hex2rgb('#4C002A'),'Markersize',15) %20k
 plot(x,A(7:10,2),'Color','k','Markersize',15) %ref
-plot(x,A(7:10,3),'Color',hex2rgb('#FF008B'),'Markersize',15) %1k
-plot(x,A(7:10,4),'Color',hex2rgb('#D10072'),'Markersize',15) %5k
+plot(x,A(7:10,3),'Color',hex2rgb('#F7AAE4'),'Markersize',15) %1k
+plot(x,A(7:10,4),'Color',hex2rgb('#FF0197'),'Markersize',15) %5k
 plot(x,A(7:10,5),'Color',hex2rgb('#B20061'),'Markersize',15) %10k
-plot(x,A(7:10,6),'Color',hex2rgb('#9C0055'),'Markersize',15) %20k
+plot(x,A(7:10,6),'Color',hex2rgb('#4C002A'),'Markersize',15) %20k
 
 hold off
-set(gca, 'XTick',1:5, 'XTickLabel',{'$0$Gy' '$5$Gy' '$10$Gy' '$15$Gy'})
+set(gca, 'XTick',1:4, 'XTickLabel',{'$0$Gy' '$5$Gy' '$10$Gy' '$15$Gy'})
 axis([0 6 10 80])
 ylabel('Damage ($\%$)')
+legend('Ref','1k','5k','10k','20k');
 
 figure(3) %45nm
 hold on
 plot(x,A(13:16,2),'.','Color','k','Markersize',15) %ref
-plot(x,A(13:16,3),'.','Color',hex2rgb('#16C3D5'),'Markersize',15) %1k
-plot(x,A(13:16,4),'.','Color',hex2rgb('#1194A2'),'Markersize',15) %5k
+plot(x,A(13:16,3),'.','Color',hex2rgb('#19FEFF'),'Markersize',15) %1k
+plot(x,A(13:16,4),'.','Color',hex2rgb('#14C5C5'),'Markersize',15) %5k
 plot(x,A(13:16,5),'.','Color',hex2rgb('#0D747F'),'Markersize',15) %10k
-plot(x,A(13:16,6),'.','Color',hex2rgb('#0B6069'),'Markersize',15) %20k
+plot(x,A(13:16,6),'.','Color',hex2rgb('#084C4C'),'Markersize',15) %20k
 plot(x,A(13:16,2),'Color','k','Markersize',15) %ref
-plot(x,A(13:16,3),'Color',hex2rgb('#16C3D5'),'Markersize',15) %1k
-plot(x,A(13:16,4),'Color',hex2rgb('#1194A2'),'Markersize',15) %5k
+plot(x,A(13:16,3),'Color',hex2rgb('#19FEFF'),'Markersize',15) %1k
+plot(x,A(13:16,4),'Color',hex2rgb('#14C5C5'),'Markersize',15) %5k
 plot(x,A(13:16,5),'Color',hex2rgb('#0D747F'),'Markersize',15) %10k
-plot(x,A(13:16,6),'Color',hex2rgb('#0B6069'),'Markersize',15) %20k
+plot(x,A(13:16,6),'Color',hex2rgb('#084C4C'),'Markersize',15) %20k
 hold off
-set(gca, 'XTick',1:5, 'XTickLabel',{'$0$Gy' '$5$Gy' '$10$Gy' '$15$Gy'})
+set(gca, 'XTick',1:4, 'XTickLabel',{'$0$Gy' '$5$Gy' '$10$Gy' '$15$Gy'})
 axis([0 6 10 80])
 ylabel('Damage ($\%$)')
+legend('Ref','1k','5k','10k','20k');
