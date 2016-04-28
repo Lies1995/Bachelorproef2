e741bc9be1c6f6578dff7905b34ebd15de885691
Lies Deceuninck <lies.deceuninck@gmail.com>
2016-04-28 22:59:55 +0200

    no message

--- Measurement_Results/gel/Dataread.m
+++ Measurement_Results/gel/Dataread.m
@@ -1,5 +1,5 @@
 set(0,'defaulttextinterpreter','latex');
-set(0,'defaultAxesFontSize', 20);
+set(0,'defaultAxesFontSize', 24);
 set(groot, 'defaultAxesTickLabelInterpreter','latex'); 
 set(groot, 'defaultLegendInterpreter','latex');
 A=100.*xlsread('finalData.xlsx');
@@ -7,12 +7,10 @@ x = 1:4;
 figure(1) %15nm
 hold on
 plot(x,A(1:4,2),'.','Color','k','Markersize',15) %ref
-plot(x,A(1:4,3),'.','Color',hex2rgb('#FFE319'),'Markersize',15) %1k
 plot(x,A(1:4,4),'.','Color',hex2rgb('#BFAA13'),'Markersize',15) %5k
 plot(x,A(1:4,5),'.','Color',hex2rgb('#87780D'),'Markersize',15) %10k
 plot(x,A(1:4,6),'.','Color',hex2rgb('#423B07'),'Markersize',15) %20k
 plot(x,A(1:4,2),'Color','k','Markersize',15) %ref
-plot(x,A(1:4,3),'Color',hex2rgb('#FFE319'),'Markersize',15) %1k
 plot(x,A(1:4,4),'Color',hex2rgb('#BFAA13'),'Markersize',15) %5k
 plot(x,A(1:4,5),'Color',hex2rgb('#87780D'),'Markersize',15) %10k
 plot(x,A(1:4,6),'Color',hex2rgb('#423B07'),'Markersize',15) %20k
@@ -21,7 +19,7 @@ hold off
 set(gca, 'XTick',1:5, 'XTickLabel',{'$0$Gy' '$5$Gy' '$10$Gy' '$15$Gy'})
 axis([0 6 0 80])
 ylabel('Damage ($\%$)')
-legend('Ref','1k','5k','10k','20k');
+legend('Ref','5k','10k','20k');
 
 figure(2) %30nm
 hold on
